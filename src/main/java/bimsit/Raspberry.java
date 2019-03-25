5f25a7293d967a5c9b49776d9169b918ca0f9d62
Xiaoyang Zhu <zxcykuaile@gmail.com>
2019-03-25 16:07:33 +0100

    raspberry

---
+++ src/main/java/bimsit/Raspberry.java
@@ -0,0 +1,88 @@
+package bimsit;
+
+import com.bushidowallet.core.bitcoin.bip32.ECKey;
+import com.bushidowallet.core.bitcoin.bip32.Seed;
+import com.bushidowallet.core.crypto.util.ByteUtil;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Arrays;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.security.Security;
+
+
+public class Raspberry
+{
+    // 50000 for mac
+    final static int loops = 1;
+    public static void raspberryOverhead()
+    {
+        Security.addProvider(new BouncyCastleProvider());
+
+        try{
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            byte[] teststr = "tx-1mesfgaejfalksdjfalskdfwje".getBytes(StandardCharsets.UTF_8);
+            long digestAccumulate = 0;
+            long digeststarter = 0;
+            long digestender = 0;
+            byte[] digest = null;
+            for(int i = 0; i < 100; i ++) {
+                digeststarter = System.nanoTime();
+                md.update(teststr);
+                digest = md.digest();
+                digestender = System.nanoTime();
+                digestAccumulate = digestAccumulate + digestender - digeststarter;
+            }
+
+            System.out.println(ByteUtil.toHex(digest));
+
+
+            System.out.println(digestAccumulate/100);
+
+            byte[] rndseed = SecureRandom.getSeed(32);
+            Mac mac = Mac.getInstance ("HmacSHA512", "BC");
+            SecretKey seedkey = new SecretKeySpec(Seed.BITCOIN_SEED.getBytes(), "HmacSHA512");
+            mac.init (seedkey);
+            byte[] lr = mac.doFinal (rndseed);
+            byte[] l = Arrays.copyOfRange(lr, 0, 32);
+            ECKey ec = new ECKey(l, true);
+
+            long sigAccumulate = 0;
+            byte[] signature = null;
+            long sigstarter = 0;
+            long sigender = 0;
+            for(int i = 0; i < loops; i ++) {
+                sigstarter = System.nanoTime();
+                signature = ec.sign(digest);
+                sigender = System.nanoTime();
+                sigAccumulate = sigAccumulate + sigender - sigstarter;
+            }
+
+            System.out.println(sigAccumulate/loops);
+
+            long verAccumulate = 0;
+            long verifystarter =0;
+            long verifyender = 0;
+            boolean verifed = false;
+            for(int i = 0; i < loops; i ++) {
+                verifystarter = System.nanoTime();
+                verifed = ec.verify(digest,signature);
+                verifyender = System.nanoTime();
+                verAccumulate = verAccumulate + verifyender - verifystarter;
+            }
+
+
+            System.out.println(verAccumulate/loops);
+            System.out.println(verifed);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/bimsit/RaspberryTest.java
@@ -0,0 +1,22 @@
+package bimsit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RaspberryTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        Raspberry rs = new Raspberry();
+        rs.raspberryOverhead();
+    }
+}
